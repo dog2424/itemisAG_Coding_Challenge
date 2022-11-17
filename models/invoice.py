454e4d62a9f788cb5b2b005807468a838d46b828
pietro24 <canevarolopietro@gmail.com>
2022-11-17 14:44:33 +0100

    add invoice total tax & grand total

--- models/invoice.py
+++ models/invoice.py
@@ -1,3 +1,4 @@
+from models.item import Item
 class Invoice():
 
     def __init__(self):
@@ -8,3 +9,20 @@ class Invoice():
         itemToBeAdded = (id, qty)
         self.invoice.append(itemToBeAdded)
 
+    def invoiceTotalTax(self):
+        invoiceTotalTax = 0
+        for value in self.invoice:
+            item = Item(value[0])
+            invoiceTotalTax += (item.tax * value[1])
+        
+        return invoiceTotalTax
+    
+    def grandTotal(self):
+        invoiceTotalTax = self.invoiceTotalTax()
+        total = 0
+        for value in self.invoice:
+            item = Item(value[0])
+            total += item.price*value[1]
+        grandTotal = invoiceTotalTax + total
+
+        return invoiceTotalTax, grandTotal
--- models/tax.py
+++
@@ -1,4 +0,0 @@
-
-
-class Tax():
-    pass
