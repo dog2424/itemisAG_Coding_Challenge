85ba2958942962bb82787fc82e851d7e3f70fdbb
pietro24 <canevarolopietro@gmail.com>
2022-11-18 11:47:30 +0100

    add unittest invoice class

--- sales_tax_unit_test.py
+++ sales_tax_unit_test.py
@@ -1,5 +1,6 @@
 import unittest
 from models.item import Item
+from models.invoice import Invoice
 
 
 class TestItem(unittest.TestCase):
@@ -26,6 +27,32 @@ class TestItem(unittest.TestCase):
         print(order.itemTaxCalculator)
 
         self.assertEqual(order.tax, 0.2)
+
+
+class TestInvoice(unittest.TestCase):
+    def AddingItems(self):
+        items = [
+            (1, "testSdTax01"),
+            (1, "testSdTax02"),
+            (1, "testSdTax03"),
+            (1, "testSdTax04"),
+        ]
+        testInv = Invoice()
+        for item in items:
+            testInv.addNewItem(item[0], item[1])
+
+        return testInv
+
+    def testInvoiceTotalTax(self):
+        testInv = self.AddingItems()
+        self.assertEqual(testInv.invoiceTotalTax(), 1.2)
+
+    def testGrandTotal(self):
+        testInv = self.AddingItems()
+
+        invoiceTotalTax, grandTotal = testInv.grandTotal()
+        self.assertEqual(invoiceTotalTax, 1.2)
+        self.assertEqual(grandTotal, 17.2)
 
 
 unittest.main()
